412b8fc0262a618dff2037823633b8fd5b45a0d7
SHINVVPP <sinsdhk@gmail.com>
2019-07-21 23:02:32 +0900

    멀티레벨 큐 스케줄러와 태스크 종료 추가

---
+++ 64비트멀티코어OS구조와원리/19_멀티레벨 큐 스케줄러와 태스크 종료/02.Kernel64/Source/InterruptHandler.h
@@ -0,0 +1,16 @@
+#ifndef __INTERRUPTHANDLER_H__
+#define __INTERRUPTHANDLER_H__
+
+#include "Types.h"
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// 함수
+//
+////////////////////////////////////////////////////////////////////////////////
+void kCommonExceptionHandler( int iVectorNumber, QWORD qwErrorCode );
+void kCommonInterruptHandler( int iVectorNumber );
+void kKeyboardHandler( int iVectorNumber );
+void kTimerHandler( int iVectorNumber );
+
+#endif /*__INTERRUPTHANDLER_H__*/
---
+++ 64비트멀티코어OS구조와원리/19_멀티레벨 큐 스케줄러와 태스크 종료/02.Kernel64/Source/Main.c
@@ -0,0 +1,103 @@
+#include "Types.h"
+#include "Keyboard.h"
+#include "Descriptor.h"
+#include "PIC.h"
+#include "Console.h"
+#include "ConsoleShell.h"
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// 라운드 로빈 스케줄러를 추가하자
+//
+////////////////////////////////////////////////////////////////////////////////
+#include "Task.h"
+#include "PIT.h"
+////////////////////////////////////////////////////////////////////////////////
+
+// 아래 함수는 C 언어 커널의 시작 부분이다.
+void Main(void)
+{
+////////////////////////////////////////////////////////////////////////////////
+//
+// 콘솔 셸 추가
+//
+////////////////////////////////////////////////////////////////////////////////
+	int iCursorX, iCursorY;
+
+    // 콘솔을 먼저 초기화한 후, 다음 작업을 수행
+    kInitializeConsole( 0, 10 ); 
+    kPrintf( "Switch To IA-32e Mode Success~!!\n" );
+    kPrintf( "IA-32e C Language Kernel Start..............[Pass]\n" );
+    kPrintf( "Initialize Console..........................[Pass]\n" );
+    
+    // 부팅 상황을 화면에 출력
+    kGetCursor( &iCursorX, &iCursorY );
+    kPrintf( "GDT Initialize And Switch For IA-32e Mode...[    ]" );
+    kInitializeGDTTableAndTSS();
+    kLoadGDTR( GDTR_STARTADDRESS );
+    kSetCursor( 45, iCursorY++ );
+    kPrintf( "Pass\n" );
+    
+    kPrintf( "TSS Segment Load............................[    ]" );
+    kLoadTR( GDT_TSSSEGMENT );
+    kSetCursor( 45, iCursorY++ );
+    kPrintf( "Pass\n" );
+    
+    kPrintf( "IDT Initialize..............................[    ]" );
+    kInitializeIDTTables();    
+    kLoadIDTR( IDTR_STARTADDRESS );
+    kSetCursor( 45, iCursorY++ );
+    kPrintf( "Pass\n" );
+    
+    kPrintf( "Total RAM Size Check........................[    ]" );
+    kCheckTotalRAMSize();
+    kSetCursor( 45, iCursorY++ );
+    kPrintf( "Pass], Size = %d MB\n", kGetTotalRAMSize() );
+    
+////////////////////////////////////////////////////////////////////////////////
+//
+// 라운드 로빈 스케줄러를 추가하자
+//
+////////////////////////////////////////////////////////////////////////////////
+    kPrintf("TCB Pool And Scheduler Initialize............[PASS]\n");
+    iCursorY++;
+    kInitializeScheduler();
+    // 1ms당 한 번씩 인터럽트가 발생하도록 설정
+    kInitializePIT(MSTOCOUNT(1),1);
+////////////////////////////////////////////////////////////////////////////////
+
+    kPrintf( "Keyboard Activate And Queue Initialize......[    ]" );
+    // 키보드를 활성화
+    if( kInitializeKeyboard() == TRUE )
+    {
+        kSetCursor( 45, iCursorY++ );
+        kPrintf( "Pass\n" );
+        kChangeKeyboardLED( FALSE, FALSE, FALSE );
+    }
+    else
+    {
+        kSetCursor( 45, iCursorY++ );
+        kPrintf( "Fail\n" );
+        while( 1 ) ;
+    }
+    
+    kPrintf( "PIC Controller And Interrupt Initialize.....[    ]" );
+    // PIC 컨트롤러 초기화 및 모든 인터럽트 활성화
+    kInitializePIC();
+    kMaskPICInterrupt( 0 );
+    kEnableInterrupt();
+    kSetCursor( 45, iCursorY++ );
+    kPrintf( "Pass\n" );
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// 멀티레벨 큐 스케줄러와 태스크 종료기능 추가
+//
+////////////////////////////////////////////////////////////////////////////////
+    // 유휴 태스크를 생성하고 셸을 시작
+    kCreateTask(TASK_FLAGS_LOWEST | TASK_FLAGS_IDLE, (QWORD)kIdleTask);
+////////////////////////////////////////////////////////////////////////////////
+
+    // 셸을 시작
+    kStartConsoleShell();
+}
