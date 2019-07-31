9878175f3dc59b22a534d71fac6916080ce8b94f
SHINVVPP <sinsdhk@gmail.com>
2019-08-01 03:53:30 +0900

    26 표준 입출력 함수 추가

---
+++ 64비트멀티코어OS구조와원리/26_C 표준 입출력 함수 추가/02.Kernel64/Source/Main.c
@@ -0,0 +1,172 @@
+#include "Types.h"
+#include "Keyboard.h"
+#include "Descriptor.h"
+#include "PIC.h"
+#include "Console.h"
+#include "ConsoleShell.h"
+////////////////////////////////////////////////////////////////////////////////
+//
+// 라운드 로빈 스케줄러를 추가하자
+//
+////////////////////////////////////////////////////////////////////////////////
+#include "Task.h"
+#include "PIT.h"
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//
+// 동적 메모리 할당
+//
+////////////////////////////////////////////////////////////////////////////////
+#include "DynamicMemory.h"
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+//
+// 하드디스크 디바이스 드라이버 추가 
+//
+////////////////////////////////////////////////////////////////
+#include "HardDisk.h"
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+//
+// 간단한 파일 시스템 구현
+//
+////////////////////////////////////////////////////////////////
+#include "FileSystem.h"
+////////////////////////////////////////////////////////////////
+
+// 아래 함수는 C 언어 커널의 시작 부분이다.
+void Main(void)
+{
+////////////////////////////////////////////////////////////////////////////////
+//
+// 콘솔 셸 추가
+//
+////////////////////////////////////////////////////////////////////////////////
+	int iCursorX, iCursorY;
+
+    // 콘솔을 먼저 초기화한 후, 다음 작업을 수행
+    kInitializeConsole( 0, 10 ); 
+    kPrintf( "Switch To IA-32e Mode Success~!!\n" );
+    kPrintf( "IA-32e C Language Kernel Start..............[Pass]\n" );
+    kPrintf( "Initialize Console..........................[Pass]\n" );
+    
+    // 부팅 상황을 화면에 출력
+    kGetCursor( &iCursorX, &iCursorY );
+    kPrintf( "GDT Initialize And Switch For IA-32e Mode...[    ]" );
+    kInitializeGDTTableAndTSS();
+    kLoadGDTR( GDTR_STARTADDRESS );
+    kSetCursor( 45, iCursorY++ );
+    kPrintf( "Pass\n" );
+    
+    kPrintf( "TSS Segment Load............................[    ]" );
+    kLoadTR( GDT_TSSSEGMENT );
+    kSetCursor( 45, iCursorY++ );
+    kPrintf( "Pass\n" );
+    
+    kPrintf( "IDT Initialize..............................[    ]" );
+    kInitializeIDTTables();    
+    kLoadIDTR( IDTR_STARTADDRESS );
+    kSetCursor( 45, iCursorY++ );
+    kPrintf( "Pass\n" );
+    
+    kPrintf( "Total RAM Size Check........................[    ]" );
+    kCheckTotalRAMSize();
+    kSetCursor( 45, iCursorY++ );
+    kPrintf( "Pass], Size = %d MB\n", kGetTotalRAMSize() );
+    
+////////////////////////////////////////////////////////////////////////////////
+//
+// 라운드 로빈 스케줄러를 추가하자
+//
+////////////////////////////////////////////////////////////////////////////////
+    kPrintf( "TCB Pool And Scheduler Initialize...........[Pass]\n");
+    iCursorY++;
+    kInitializeScheduler();
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// 동적 메모리 할당
+//
+////////////////////////////////////////////////////////////////////////////////
+// 동적 메모리 초기화
+    kPrintf("Dynamic Memory Initialize...................[Pass]\n");
+    iCursorY++;
+    kInitializeDynamicMemory();
+////////////////////////////////////////////////////////////////////////////////
+
+    // 1ms당 한 번씩 인터럽트가 발생하도록 설정
+    kInitializePIT(MSTOCOUNT(1),1);
+
+    kPrintf( "Keyboard Activate And Queue Initialize......[    ]" );
+    // 키보드를 활성화
+    if( kInitializeKeyboard() == TRUE )
+    {
+        kSetCursor( 45, iCursorY++ );
+        kPrintf( "Pass\n" );
+        kChangeKeyboardLED( FALSE, FALSE, FALSE );
+    }
+    else
+    {
+        kSetCursor( 45, iCursorY++ );
+        kPrintf( "Fail\n" );
+        while( 1 ) ;
+    }
+    
+    kPrintf( "PIC Controller And Interrupt Initialize.....[    ]" );
+    // PIC 컨트롤러 초기화 및 모든 인터럽트 활성화
+    kInitializePIC();
+    kMaskPICInterrupt( 0 );
+    kEnableInterrupt();
+    kSetCursor( 45, iCursorY++ );
+    kPrintf( "Pass\n" );
+
+////////////////////////////////////////////////////////////////
+//
+// 하드디스크 디바이스 드라이버 추가 
+//
+////////////////////////////////////////////////////////////////
+// 하드 디스크를 초기화
+    kPrintf( "HDD Initialize..............................[    ]" );
+    if( kInitializeHDD() == TRUE )
+    {
+        kSetCursor( 45, iCursorY++ );
+        kPrintf( "Pass\n" );
+    }
+    else
+    {
+        kSetCursor( 45, iCursorY++ );
+        kPrintf( "Fail\n" );
+    }
+////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////
+//
+// 간단한 파일 시스템 구현
+//
+////////////////////////////////////////////////////////////////
+    // 파일 시스템을 초기화
+    kPrintf( "File System Initialize......................[    ]" );
+    if( kInitializeFileSystem() == TRUE )
+    {
+        kSetCursor( 45, iCursorY++ );
+        kPrintf( "Pass\n" );
+    }
+    else
+    {
+        kSetCursor( 45, iCursorY++ );
+        kPrintf( "Fail\n" );
+    }
+////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// 멀티 스레딩 기능을 추가하자.
+//
+////////////////////////////////////////////////////////////////////////////////
+    // 유휴 태스크를 시스템 스레드로 생성하고 셸을 시작
+    kCreateTask( TASK_FLAGS_LOWEST | TASK_FLAGS_THREAD | TASK_FLAGS_SYSTEM | TASK_FLAGS_IDLE, 0, 0, 
+            ( QWORD ) kIdleTask );
+    kStartConsoleShell();
+////////////////////////////////////////////////////////////////////////////////
+}
